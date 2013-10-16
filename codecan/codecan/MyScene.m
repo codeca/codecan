bff3d9da5ec237ce5bcc0973dbb81895c9ef2749
Otávio Netto Zani <otavionettozani@gmail.com>
2013-10-16 15:43:16 -0300

    teste ot

--- codecan/codecan/MyScene.m
+++ codecan/codecan/MyScene.m
@@ -20,7 +20,7 @@
         
         SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
         
-        myLabel.text = @"Hello, World!";
+        myLabel.text = @"Hello, World teste!";
         myLabel.fontSize = 30;
         myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
                                        CGRectGetMidY(self.frame));

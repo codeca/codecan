b5a534bb2715b48141558cb80312c9063ac9d9c7
Guilherme Souza <guilhermesouza@Guilherme-Souza.local>
2013-10-16 13:51:38 -0300

    Initial files

---
+++ codecan/codecanTests/codecanTests.m
@@ -0,0 +1,34 @@
+//
+//  codecanTests.m
+//  codecanTests
+//
+//  Created by Guilherme Souza on 10/16/13.
+//  Copyright (c) 2013 Codeca. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface codecanTests : XCTestCase
+
+@end
+
+@implementation codecanTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

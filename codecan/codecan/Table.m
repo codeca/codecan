3e3ea16f156a40c246a3fb55576a72df1e4856aa
Sylviomoc <sylviocardoso@MacBook-Pro-de-Sylvio.local>
2013-10-23 08:39:27 -0200

    Table code commented
    Someone take a look at it!

--- codecan/codecan/Table.m
+++ codecan/codecan/Table.m
@@ -11,50 +11,55 @@
 @implementation Table
 
 -(id)init{
+	
+/*
+ 
+ Não apaguei o código porque algo aqui ainda pode ser útil. Dêem uma olhada certinho, ok?
+ 
+ */
 
-	self = [super init];
-	
-	if(self){
-	
-		for(int i =0; i < 19; i ++){
-			HexagonNode * newhex = [HexagonNode spriteNodeWithImageNamed:@"tile"];
-			newhex.number = i;
-			
-			
-				
-				int x0= 768/2;
-				int y0= 1024/2+44;
-				newhex.name = [NSString stringWithFormat:@"%i", i];
-				
-				if(i<1)
-					newhex.position = CGPointMake(x0, y0);
-				if(i>=1 && i<7)
-					newhex.position = CGPointMake(x0+133*cos((i-1)*M_PI/3), y0+133*sin((i-1)*M_PI/3));
-				if(i>=7 && !(i%2))
-					newhex.position = CGPointMake(x0+230*cos((i-7)*M_PI/6), y0+230*sin((i-7)*M_PI/6));
-				else if(i>=7)
-					newhex.position = CGPointMake(x0+266*cos((i-7)*M_PI/6), y0+266*sin((i-7)*M_PI/6));
-			
-				[self.hexes addObject:newhex];
-		}
-		
-<<<<<<< HEAD
-		for(HexagonNode *hex in self.hexes){
-=======
-		for(int i =0; i < 54; i++){
-			
-		}
-		
-		for(HexModel *hex in self.hexes){
->>>>>>> 53bb2c89aa3144c3e96176a315ca8642ddf420be
-			
-			for(int i =0; i<6;i++){
-				
-			}
-		
-		}
-		
-	}
+//	self = [super init];
+//	
+//	if(self){
+//	
+//		for(int i =0; i < 19; i ++){
+//			HexagonNode * newhex = [HexagonNode spriteNodeWithImageNamed:@"tile"];
+//			newhex.number = i;
+//			
+//			
+//				
+//				int x0= 768/2;
+//				int y0= 1024/2+44;
+//				newhex.name = [NSString stringWithFormat:@"%i", i];
+//				
+//				if(i<1)
+//					newhex.position = CGPointMake(x0, y0);
+//				if(i>=1 && i<7)
+//					newhex.position = CGPointMake(x0+133*cos((i-1)*M_PI/3), y0+133*sin((i-1)*M_PI/3));
+//				if(i>=7 && !(i%2))
+//					newhex.position = CGPointMake(x0+230*cos((i-7)*M_PI/6), y0+230*sin((i-7)*M_PI/6));
+//				else if(i>=7)
+//					newhex.position = CGPointMake(x0+266*cos((i-7)*M_PI/6), y0+266*sin((i-7)*M_PI/6));
+//			
+//				[self.hexes addObject:newhex];
+//		}
+//		
+//
+//		for(HexagonNode *hex in self.hexes){
+//
+//		for(int i =0; i < 54; i++){
+//			
+//		}
+//		
+//		for(HexagonNode *hex in self.hexes){
+//			
+//			for(int i =0; i<6;i++){
+//				
+//			}
+//		
+//		}
+//		
+//	}
 	
 	return self;
 

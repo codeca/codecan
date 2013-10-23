e5f83729e9355b1c15bbc64c57fd07ebee973f80
Otávio Netto Zani <otavionettozani@gmail.com>
2013-10-23 10:07:29 -0200

    ARRUMANDO BUGS

--- codecan/codecan/VertexNode.h
+++ codecan/codecan/VertexNode.h
@@ -7,23 +7,26 @@
 //
 
 #import <SpriteKit/SpriteKit.h>
+#import "Port.h"
 
-enum vertexType {
+typedef enum {
 	
 	village = 1,
 	city = 2
 	
-};
+} VertexType;
 
 @interface VertexNode : SKSpriteNode
 
+<<<<<<< HEAD
 //@property (nonatomic, strong) Player* owner;
+=======
+@property (nonatomic, weak) Player* owner;
+>>>>>>> e156045a996fb242f026b548ab07c620b87cf235
 
-@property (nonatomic) enum vertexType type;
+@property (nonatomic) VertexType type;
 
-@property (nonatomic, strong) NSArray* hexagons;
-
-@property (nonatomic,strong) NSArray* edges;
+@property (nonatomic,strong) NSMutableArray* edges;
 
 //@property (nonatomic, strong) Port* port;
 

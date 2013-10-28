19c6f0ae762727cb0dc879aef5cb1b3f76f29ea0
Otávio Netto Zani <otavionettozani@gmail.com>
2013-10-28 14:27:17 -0200

    Connect Server Various Players Message Handling

--- codecan/codecan/StartViewController.m
+++ codecan/codecan/StartViewController.m
@@ -10,10 +10,17 @@
 
 @interface StartViewController ()
 
+typedef enum {
+	WAITING_3 = 0,
+	WAITING_4,
+	WAITING_3_4
+}waitingFor;
 
 @property (nonatomic, strong) Plug* plug;
 @property (weak, nonatomic) IBOutlet UILabel *progressText;
 @property (weak, nonatomic) IBOutlet UITextField *name;
+@property (nonatomic) waitingFor waiting;
+@property (nonatomic, strong) NSString* myId;
 
 @end
 
@@ -24,12 +31,15 @@
 	
 	self.plug = [[Plug alloc] init];
 	self.plug.delegate = self;
+	self.myId = [[NSUUID UUID] UUIDString];
+	
 }
 
 
 - (IBAction)startWith4Players:(id)sender {
 	NSDictionary *serverData = [NSDictionary dictionaryWithObjects:@[@0, @1, self.name.text, [[NSUUID UUID] UUIDString]] forKeys:@[@"want3", @"want4", @"name", @"id"]];
 	
+	self.waiting = WAITING_4;
 	[self.plug sendMessage:MSG_MATCH data:serverData];
 	
 
@@ -37,14 +47,30 @@
 
 
 - (IBAction)startWith3Players:(id)sender {
-	NSDictionary *serverData = [NSDictionary dictionaryWithObjects:@[@1, @0, self.name.text, [[NSUUID UUID] UUIDString]] forKeys:@[@"want3", @"want4", @"name", @"id"]];
-	
+	NSDictionary *serverData = [NSDictionary dictionaryWithObjects:@[@1, @0, self.name.text, self.myId] forKeys:@[@"want3", @"want4", @"name", @"id"]];
+	self.waiting = WAITING_3;
 	[self.plug sendMessage:MSG_MATCH data:serverData];
 	
 }
 
 - (void)plug:(Plug *)plug receivedMessage:(PlugMsgType)type data:(id)data {
 	NSLog(@"received %d %@", type, data);
+	
+	
+	if(type == MSG_MATCH_PROGRESS){
+		NSInteger waiting3 = [[data objectForKey:@"waitingFor3"] integerValue];
+		NSInteger waiting4 =[[data objectForKey:@"waitingFor4"] integerValue];
+		
+		if(self.waiting == WAITING_3)
+			self.progressText.text = [NSString stringWithFormat:@"Waiting for %i more players", 3-waiting3];
+		else
+			self.progressText.text = [NSString stringWithFormat:@"Waiting for %i more players", 4-waiting4];
+	}
+	
+	else if(type == MSG_MATCH_DONE){
+		
+		
+	}
 
 
 }
@@ -57,7 +83,7 @@
 
 - (void)plug:(Plug *)plug hasClosedWithError:(BOOL)error {
 	NSLog(@"closed %hhd", error);
-
+	
 
 }
 

bbf97e9f3f5d81a3d3b55c6629539f26dd6bea92
Otávio Netto Zani <otavionettozani@gmail.com>
2013-10-30 14:45:39 -0200

    New DevelopmentCards

---
+++ codecan/codecan/DevelopmentCards.h
@@ -0,0 +1,13 @@
+//
+//  DevelopmentCards.h
+//  codecan
+//
+//  Created by Otávio Netto Zani on 30/10/13.
+//  Copyright (c) 2013 Codeca. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface DevelopmentCards : SKNode
+
+@end
---
+++ codecan/codecan/DevelopmentCards.m
@@ -0,0 +1,13 @@
+//
+//  DevelopmentCards.m
+//  codecan
+//
+//  Created by Otávio Netto Zani on 30/10/13.
+//  Copyright (c) 2013 Codeca. All rights reserved.
+//
+
+#import "DevelopmentCards.h"
+
+@implementation DevelopmentCards
+
+@end
